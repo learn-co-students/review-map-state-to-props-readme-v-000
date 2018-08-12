b5c898c9b02d3cb2289548421888a676665c4b9b
ecssiah <ecssiah@gmail.com>
2018-08-11 19:21:17 -0500

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -6,12 +6,10 @@ export default function shoppingListItemReducer(
       return Object.assign({}, state, {
         items: state.items.concat(state.items.length + 1)
       });
-
     case 'GET_COUNT_OF_USERS':
       return Object.assign({}, state, {
         users: state.users.concat(state.users.length + 1)
       });
-
     default:
       return state;
   }

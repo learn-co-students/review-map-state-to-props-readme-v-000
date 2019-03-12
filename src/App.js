b5f8e6a30aaed2506373aef4ae13a5472df5fee9
Brenden Thornton <thorntonbrenden@gmail.com>
2019-03-12 17:42:45 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ class App extends Component {
 }
 
 const mapStateToProps = (state) => {
-  debugger;
   return { items: state.items }
 }
 
 export default connect(mapStateToProps)(App);
+// export default connect( state => ({ items: state.items }) )(App);

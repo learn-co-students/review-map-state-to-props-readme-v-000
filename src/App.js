7f61fe52aa5d4fca7478c95e47a8140dcbf53332
github username <github email address>
2019-04-24 19:46:05 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,32 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import './App.css';
-
+import { connect } from 'react-redux';
+import { addItem } from  './actions/items';
 
 class App extends Component {
 
-  handleOnClickItems() {
-    this.props.store.dispatch({
-      type: 'GET_COUNT_OF_ITEMS',
-    });
-  }
-
-  handleOnClickUsers() {
-    this.props.store.dispatch({
-      type: 'GET_COUNT_OF_USERS',
-    });
+  handleOnClick = event => {
+    this.props.addItem()
   }
 
   render() {
-    // debugger;
+    debugger
     return (
       <div className="App">
-          <button onClick={() => this.handleOnClickItems()}>
-            Click to change items count
-            </button>
-          <button onClick={() => this.handleOnClickUsers()}>
-            Click to change user count
+        <button onClick={this.handleOnClick}>
+          Click
           </button>
-          <p>{this.props.items.length}</p>
+        <p>{this.props.items.length}</p>
       </div>
     );
   }
-}
+};
 
 // const mapStateToProps = (state) => {
-//   debugger;
-//   return { items: state.items }
-// }
-//
-// export default connect(mapStateToProps)(App);
+//   return {
+//     items: state.items
+//   };
+// };
 
-// const vanilla = (milkshake) => {
-//   debugger;
-//   return { items: milkshake.items }
-// }
-
-// export default connect(vanilla)(App);
-export default connect( state => ({ items: state.items }) )(App);
+export default connect(state => ({ items: state.items }), { addItem })(App);
+/* ES6 shorthand lets us pass in *one* value that will be read as the key and value */
